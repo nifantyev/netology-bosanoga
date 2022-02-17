9029f0d2f0ec5cc985be4f762cea50c7496e329a
Евгений Нифантьев <nifantyev@gmail.com>
2022-02-17 13:26:56 +0300

    Добавил TopSales на главную страницу

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,74 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../store';
+import { fetchTopSales } from '../reducers/topSalesReducer';
 import Banner from '../components/Banner';
+import ProductCard from '../components/ProductCard';
+import TopSales from '../components/TopSales';
 
 export default function MainPage() {
+  const dispatch = useAppDispatch();
+  const topSalesLoadingStatus = useAppSelector(
+    (store) => store.topSales.loadingStatus
+  );
+  const topSalesProducts = useAppSelector((store) => store.topSales.products);
+
+  useEffect(() => {
+    dispatch(fetchTopSales());
+  }, [dispatch]);
+
   return (
     <main className="container">
       <div className="row">
         <div className="col">
           <Banner />
-          <section className="top-sales">
-            <h2 className="text-center">Хиты продаж!</h2>
-            <div className="row">
-              <div className="col-4">
-                <div className="card">
-                  <img
-                    src="./img/products/sandals_myer.jpg"
-                    className="card-img-top img-fluid"
-                    alt="Босоножки 'MYER'"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">Босоножки 'MYER'</p>
-                    <p className="card-text">34 000 руб.</p>
-                    <a
-                      href="/products/1.html"
-                      className="btn btn-outline-primary"
-                    >
-                      Заказать
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="card">
-                  <img
-                    src="./img/products/sandals_keira.jpg"
-                    className="card-img-top img-fluid"
-                    alt="Босоножки 'Keira'"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">Босоножки 'Keira'</p>
-                    <p className="card-text">7 600 руб.</p>
-                    <a
-                      href="/products/1.html"
-                      className="btn btn-outline-primary"
-                    >
-                      Заказать
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="card">
-                  <img
-                    src="./img/products/superhero_sneakers.jpg"
-                    className="card-img-top img-fluid"
-                    alt="Супергеройские кеды"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">Супергеройские кеды</p>
-                    <p className="card-text">1 400 руб.</p>
-                    <a
-                      href="/products/1.html"
-                      className="btn btn-outline-primary"
-                    >
-                      Заказать
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+          <TopSales
+            products={topSalesProducts}
+            isLoading={topSalesLoadingStatus === 'pending'}
+            isError={topSalesLoadingStatus === 'error'}
+          />
           <section className="catalog">
             <h2 className="text-center">Каталог</h2>
             <ul className="catalog-categories nav justify-content-center">
@@ -100,23 +57,13 @@ export default function MainPage() {
             </ul>
             <div className="row">
               <div className="col-4">
-                <div className="card catalog-item-card">
-                  <img
-                    src="./img/products/sandals_myer.jpg"
-                    className="card-img-top img-fluid"
-                    alt="Босоножки 'MYER'"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">Босоножки 'MYER'</p>
-                    <p className="card-text">34 000 руб.</p>
-                    <a
-                      href="/products/1.html"
-                      className="btn btn-outline-primary"
-                    >
-                      Заказать
-                    </a>
-                  </div>
-                </div>
+                <ProductCard
+                  id={1}
+                  title="Босоножки 'MYER'"
+                  image="./img/products/sandals_myer.jpg"
+                  price={34000}
+                  addClassNames="catalog-item-card"
+                />
               </div>
               <div className="col-4">
                 <div className="card catalog-item-card">
