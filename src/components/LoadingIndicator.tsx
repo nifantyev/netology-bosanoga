47ad3f3597acf2ef9c7450b7b69c8681f94db9c3
Евгений Нифантьев <nifantyev@gmail.com>
2022-02-17 11:13:04 +0300

    Реализовал LoadingIndicator

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,10 @@
+export default function LoadingIndicator() {
+  return (
+    <div className="preloader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
