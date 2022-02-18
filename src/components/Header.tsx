f311644c1058aac80ef5a6ec205c9019aa2e4ae1
Евгений Нифантьев <nifantyev@gmail.com>
2022-02-18 12:54:15 +0300

    Реализовал поиск из шапки сайта

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../store';
+import {
+  clearProducts,
+  fetchProducts,
+  selectCategory,
+  setSearch,
+} from '../reducers/productsReducer';
 import logo from '../img/header-logo.png';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useAppDispatch();
+
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const search = () => {
+    dispatch(setSearch(searchText));
+    setSearchText('');
+    setSearchVisible(false);
+    if (location.pathname !== '/catalog.html') {
+      navigate('/catalog.html');
+    } else {
+      dispatch(selectCategory(undefined));
+      dispatch(clearProducts());
+      dispatch(fetchProducts());
+    }
+  };
+
+  const handleSearchClick = () => {
+    if (!searchText) {
+      setSearchVisible((prev) => !prev);
+    } else {
+      search();
+    }
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    search();
+  };
+
   return (
     <header className="container">
       <div className="row">
         <div className="col">
           <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <a className="navbar-brand" href="/">
+            <Link to="/" className="navbar-brand">
               <img src={logo} alt="Bosa Noga" />
-            </a>
+            </Link>
             <div className="collapase navbar-collapse" id="navbarMain">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
@@ -59,6 +103,7 @@ export default function Header() {
                   <div
                     data-id="search-expander"
                     className="header-controls-pic header-controls-search"
+                    onClick={handleSearchClick}
                   ></div>
                   <div className="header-controls-pic header-controls-cart">
                     <div className="header-controls-cart-full">1</div>
@@ -67,9 +112,17 @@ export default function Header() {
                 </div>
                 <form
                   data-id="search-form"
-                  className="header-controls-search-form form-inline invisible"
+                  className={`header-controls-search-form form-inline ${
+                    !searchVisible ? 'invisible' : ''
+                  }`}
+                  onSubmit={handleSubmit}
                 >
-                  <input className="form-control" placeholder="Поиск" />
+                  <input
+                    className="form-control"
+                    placeholder="Поиск"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                  />
                 </form>
               </div>
             </div>
